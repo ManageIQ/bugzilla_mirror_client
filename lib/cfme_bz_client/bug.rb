6327ec6a6522b18e38653d397487cfabbd618974
Brandon Dunne <bdunne@redhat.com>
2014-06-20 16:49:41 -0400

    [CfmeBzClient] CfmeBzClient::Bug Add attributes for things used in builder

--- lib/cfme_bz_client/bug.rb
+++ lib/cfme_bz_client/bug.rb
@@ -1,11 +1,27 @@
 class CfmeBzClient
   class Bug
-
     attr_accessor :attributes
+    attr_accessor :blocks
+    attr_accessor :clones
+    attr_accessor :depends_on
+    attr_accessor :fixed_in
+    attr_accessor :flags
     attr_accessor :id
+    attr_accessor :keywords
+    attr_accessor :status
+    attr_accessor :target_release
 
     def initialize(attribute_set)
-      @id         = attribute_set.delete("bug_id")
+      @blocks         = attribute_set.delete("blocks")
+      @clones         = attribute_set.delete("clones")
+      @depends_on     = attribute_set.delete("depends_on")
+      @fixed_in       = attribute_set.delete("fixed_in")
+      @flags          = attribute_set.delete("flags")
+      @id             = attribute_set.delete("bug_id")
+      @keywords       = attribute_set.delete("keywords")
+      @status         = attribute_set.delete("status")
+      @target_release = attribute_set.delete("target_release")
+
       @attributes = attribute_set
     end
   end
